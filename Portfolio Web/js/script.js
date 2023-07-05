bab50a785cb774a09b467cdb0c60d26f545ba950
Vaidikramani0509 <105126631+Vaidikramani0509@users.noreply.github.com>
2023-07-05 18:07:19 +0530

    Complete

---
+++ Portfolio Web/js/script.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".animationtext", {
+    strings: ["frontend developer", "Youtuber", "web developer"],
+    typeSpeed: 100,
+    backSpeed: 50,
+    backDelay: 1000,
+    loop: true
+});
