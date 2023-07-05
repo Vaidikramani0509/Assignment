83557bee59bef2008fde22e7ed71e07ff7741e32
Vaidikramani0509 <105126631+Vaidikramani0509@users.noreply.github.com>
2023-07-05 18:06:11 +0530

    Delete AWT Fullstack Internship directory

--- AWT Fullstack Internship/script.js
+++
@@ -1,7 +0,0 @@
-var typed = new Typed(".animationtext", {
-    strings: ["frontend developer", "Youtuber", "web developer"],
-    typeSpeed: 100,
-    backSpeed: 50,
-    backDelay: 1000,
-    loop: true
-});
